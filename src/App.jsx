4f0e4cb116ad28906e52e0a34fc7b7deee0b442f
ReRun007 <117578115+ReRun007@users.noreply.github.com>
2024-10-16 00:17:55 +0700

    Update App.jsx
    เสร็จจริงๆ

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,16 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-import { Link } from 'react-router-dom'
-import Todo from './components/Todo'
+import { useEffect } from 'react'
+import { useNavigate } from 'react-router-dom'
 
 function App() {
+  const navigate = useNavigate();
 
-  return (
-    <>
-      <h3>Wellcom</h3>
-      <img src={reactLogo} alt="" />
-      <br /><br />
-      <Link to="/login" className='btn btn-success'>Login</Link>
-      <Link to="/register" className='btn btn-primary'>Register</Link>
-      <Todo />
-      <Link to="/dashboard" className='btn btn-primary'>แดชบอร์ด</Link>
-    </>
-  )
+  useEffect(() => {
+    // Redirect to login page when the component mounts
+    navigate('/login');
+  }, [navigate]);
+
+  // This component doesn't render anything
+  return null;
 }
 
 export default App
